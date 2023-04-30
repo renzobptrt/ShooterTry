136169c21362dff7d542de0e15dc013e34e3e874
renzobptrt <renzobptrt@gmail.com>
2023-04-30 01:45:28 -0500

    first commit

---
+++ Source/ShooterCourse/ShooterCharacter.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SHOOTERCOURSE_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	/* Called for forwards/backwards input */
+	void MoveForward(float Value);
+	/* Called for right/left input*/
+	void MoveRight(float Value);
+
+	/*
+	* Called via input to turn at a given rate
+	* @param Rate This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnAtRate(float Rate);
+
+	/**
+	* Called via input to look up/down at a given rate
+	* @param Rate This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void LookUpAtRate(float Rate);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//How to show the variable on the editor
+	/** Camera boom positioning the camera behind the character*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+	
+	/*Camera that follows the character*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+	
+	// Base turn rate, in deg/sec. Other scaling may effect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	//Base look up/down rate, in deg/sec. Other scaling may effect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+
+public:
+	/** Returns CameraBoom subobject*/
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns Followcamaera subobject*/
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
---
+++ Source/ShooterCourse/ShooterCourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterCourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERCOURSE_API AShooterCourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
